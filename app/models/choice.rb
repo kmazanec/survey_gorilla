74b26128411cb61983a802539c3588577e67870b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 16:08:50 -0500

    associations should work

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,6 @@
 class Choice < ActiveRecord::Base
-  belongs_to :question, through: :options
   belongs_to :option
+  has_one :question, through: :option
   belongs_to :taken_survey
-  belongs_to :user, through: :taken_survey
+  has_one :taker, through: :taken_survey
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,6 @@
 class Survey < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, class_name: "User"
   has_many :questions
+  has_many :taken_surveys
+  has_many :takers, through: :taken_surveys
 end
