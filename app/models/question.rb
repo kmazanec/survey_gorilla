d7784ede498acb6775edd7ece2fba0bbdde69392
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 17:44:09 -0500

    one val

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,18 @@
 class Question < ActiveRecord::Base
+
   belongs_to :survey
   has_many :options
   has_many :choices, through: :options
+
+  validates :text, presence: true
+  validate :two_options
+
+
+    def two_options
+      if self.options.length < 2
+        errors.add(:options, "Question must have at least two options!") 
+      end
+
+    end  
+
 end
