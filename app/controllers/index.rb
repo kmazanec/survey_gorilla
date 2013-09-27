38c1297dae328389b916b1365fb765c58cf1b282
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 15:49:21 -0500

    filled out more views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,56 @@
+
+# ====== GET ROUTES ========================
+
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create_survey' do
+
+  erb :create_survey
+end
+
+
+get '/profile' do
+
+  erb :profile
+end
+
+get '/login' do
+
+  erb :login
+end
+
+get '/survey/:survey_id' do
+
+  erb :take_survey
+end
+
+
+get '/survey/:survey_id/results' do
+
+  erb :survey_results
+end
+
+
+# ====== POST ROUTES ========================
+
+
+post '/login' do
+
+
+end
+
+post '/survey/:survey_id' do
+
+
+end
+
+post '/signup' do
+
+
+end
+
+
