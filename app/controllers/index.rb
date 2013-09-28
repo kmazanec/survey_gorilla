e5bc9a3b39d2c29d432af43871131007751a9872
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 19:37:52 -0500

    post route for create survey mostly doneish

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,9 +16,6 @@ end
 
 get '/create_survey' do
   if session[:user_id]
-
-
-
     erb :create_survey
   else
     redirect to "/login"
@@ -80,6 +77,24 @@ post '/create_survey' do
   puts params.inspect
   puts "======================================"
   puts
+
+  new_survey = Survey.new(name: params[:name], creator_id: session[:user_id])
+  new_question = Question.new(text: params[:question])
+  
+  params[:options].each_value do |option_text|
+    new_question.options << Option.create(text: option_text)
+  end
+
+  new_question.save
+  new_survey.questions << new_question
+
+  new_survey.save
+
+  puts "======================================"
+  puts "New Survey: #{new_survey.inspect}"
+  puts "New Question: #{new_question.inspect}"
+  puts "New question's options: #{new_question.options.inspect}"
+  puts "======================================"
 
 
 
